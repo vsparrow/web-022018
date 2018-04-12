c3b75baeeae936ca1ee3abf6264682b727a81e7b
vsparrow <khan@vsparrow.com>
2018-04-12 13:02:09 -0400

    create task working

--- 28-javascript-practice/front-end/src/index.js
+++ 28-javascript-practice/front-end/src/index.js
@@ -11,7 +11,7 @@ function displayTop(){
   //button for all
   let div = `<div id="toolbar">`
   div += ` <button type="button" onclick="startDisplayAllTasks()">SHOW ALL!</button> `
-  div += ` <button type="button" onclick="createNew()">CREATE NEW!</button> `
+  div += ` <button type="button" onclick="startCreateNew()">CREATE NEW!</button> `
   div+=`</div>`
   let top = document.querySelector("#top")
   top.innerHTML = "";
@@ -47,11 +47,16 @@ function displayAllTasks(data){
     let li = document.createElement('li')
     li.setAttribute('data-id',el.id)
     li.textContent = `Description:${el.description} ::::: Priority:${el.priority} ::::: Completed:${el.completed}`
-    /////////add a button to li
+    /////////add a mark complete button to li
     let completedButton = document.createElement('button')
     completedButton.textContent = "Mark Completed"
     completedButton.addEventListener("click",()=>{setTaskCompleted(el.id)})
     li.appendChild(completedButton)
+    //////// delete button
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = "DELETE"
+    deleteButton.addEventListener("click",()=>{deleteTask(el.id)})
+    li.appendChild(deleteButton)
     ////////
     dataDiv.appendChild(li)
     // li.addEventListener("click",()=>{editTask(el.id)})
@@ -74,6 +79,45 @@ function setTaskCompleted(id){
   }).then(res=>res.json())//.then(json=>console.log(json))
   .then(json=>startDisplayAllTasks())
 }//setTaskCompleted
-// Add a task
-// Check a task as complete
-// Remove a task
+
+function deleteTask(id){
+  console.log("deleteTask:" +id);
+  let url = TASKSURL + "/" +id
+  fetch(url,{method: "DELETE"}).then(res=>startDisplayAllTasks())
+    // .then(res=>res.json())
+    // .then(json=>console.log(json))
+  console.log("done with delete of " + id);
+}//deleteTask
+
+function startCreateNew(){
+  let description = document.createElement('input')
+  description.type = 'text'
+  description.setAttribute('placeholder','description')
+  description.setAttribute('id','newdescription')
+  let priority = document.createElement('input')
+  priority.type = 'text'
+  priority.setAttribute('placeholder','priority')
+  priority.setAttribute('id','newpriority')
+  let submitButton = document.createElement('button')
+  submitButton.textContent = "SUBMIT"
+  submitButton.addEventListener("click",()=>{createTask()})////////////
+  document.querySelector("body").appendChild(description)
+  document.querySelector("body").appendChild(priority)
+  document.querySelector("body").appendChild(submitButton)
+  // let li = document.createElement('li')
+  // li.setAttribute('data-id',el.id)
+
+}//end startCreateNew
+
+function createTask(){
+let description = document.querySelector("#newdescription").value
+let priority = document.querySelector("#newpriority").value
+fetch(TASKSURL,{
+  method: "POST",
+  headers: {'Content-type': 'application/json'},
+  body: JSON.stringify({description:description, priority:priority})
+})
+.then(res=>res.json())
+// .then(json=>console.log(json))
+.then(json=>startDisplayAllTasks())
+}
